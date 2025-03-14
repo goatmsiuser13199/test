5ca6f74f7508a9f1c7909303ff86e5c9fca747f5
msiusergoat <xhjg44x@gmail.com>
2025-03-14 11:55:10 +0100

    No changes made to the codebase.

--- front/app/logoutButton.tsx
+++ front/app/logoutButton.tsx
@@ -6,7 +6,7 @@ export default async function LogoutButton() {
         redirect("/logout")
     }
     return (
-        <form className="bg-[ffff] text-[#ffff] flex flex-col justify-center items-center p-4 rounded-lg overflow-hidden absolute top-5 right-8 cursor-pointer" action={clickButton}>
+        <form className="bg-red-500 text-white flex flex-col justify-center items-center p-4 rounded-lg overflow-hidden absolute top-5 right-8 cursor-pointer" action={clickButton}>
             <input type="submit" className="w-full h-full cursor-pointer bg-red-500 text-white" value="Logout" />
         </form>
     )
