c6b3cac71ec3d0936add5fa32660e19364f6cf2f
msiusergoat <xhjg44x@gmail.com>
2025-03-14 11:53:19 +0100

    palu

--- front/app/logoutButton.tsx
+++ front/app/logoutButton.tsx
@@ -6,8 +6,8 @@ export default async function LogoutButton() {
         redirect("/logout")
     }
     return (
-        <form className="bg-[#64BDC2] text-[#ffff] flex flex-col justify-center items-center p-4 rounded-lg overflow-hidden absolute top-5 right-8 cursor-pointer" action={clickButton}>
-            <input type="submit" className="w-full h-full cursor-pointer" value="Logout" />
+        <form className="bg-[ffff] text-[#ffff] flex flex-col justify-center items-center p-4 rounded-lg overflow-hidden absolute top-5 right-8 cursor-pointer" action={clickButton}>
+            <input type="submit" className="w-full h-full cursor-pointer bg-red-500 text-white" value="Logout" />
         </form>
     )
 }
